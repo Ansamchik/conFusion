40f6b5987bb5f7125a0f2b09c77d67fb611dfc1a
Ansam <b707an@list.ru>
2017-10-30 14:35:04 +0300

    Reactive Forms Part II

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,12 +7,14 @@ import { Feedback, ContactType } from '../shared/feedback';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
+
 export class ContactComponent implements OnInit {
 
   feedbackForm: FormGroup;
   feedback: Feedback;
   contactType = ContactType;
-
+  pureEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  phone = '[\+][0-9() ]{7,}$';
   constructor(private fb: FormBuilder) {
     this.createForm();
    }
@@ -22,10 +24,10 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      telnum: ['' , [Validators.required, Validators.pattern(this.phone)]],
+      email: ['', [Validators.required, Validators.email, Validators.pattern(this.pureEmail)]],
       agree: false,
       contacttype: 'None',
       message: ''
@@ -35,7 +37,15 @@ export class ContactComponent implements OnInit {
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+        firstname: '',
+        lastname: '',
+        telnum: '',
+        email: '',
+        agree: false,
+        contacttype: 'None',
+        message: ''
+    });
   }
 
 }
