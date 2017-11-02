5d341c6522d2b9a6078bed68e4f8850e22ae3511
Ansam <b707an@list.ru>
2017-11-02 13:50:11 +0300

    RxJS Part I

--- src/app/services/leaders.service.ts
+++ src/app/services/leaders.service.ts
@@ -2,30 +2,26 @@ import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/of';
+
 @Injectable()
 export class LeadersService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]> {
-      return new Promise(resolve => {
-          // Simulate server latency with 2 second delay
-          setTimeout(() => resolve(LEADERS), Math.random() * 2000);
-      });
+  getLeaders(): Observable<Leader[]> {
+      return Observable.of(LEADERS).delay(Math.random() * 2000);
   }
 
-  getLeaderById(id: number): Promise<Leader> {
-      return new Promise(resolve => {
-          // Simulate server latency with 2 second delay
-          setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), Math.random() * 2000);
-      });
+  getLeaderById(id: number): Observable<Leader> {
+      return Observable.of(LEADERS.filter((leader) => (leader.id === id))[0]).delay(Math.random() * 2000);
   }
 
-  getFeaturedLeader(): Promise<Leader> {
-      return new Promise(resolve => {
-          // Simulate server latency with 2 second delay
-          setTimeout(() => resolve(LEADERS.filter((leader) => (leader.featured))[0]), Math.random() * 2000);
-      });
+  getFeaturedLeader(): Observable<Leader> {
+      return Observable.of(LEADERS.filter((leader) => (leader.featured))[0]).delay(Math.random() * 2000);
   }
 
 }
